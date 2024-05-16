22301c32ceaaeb7639d9d592eefd838f1561be16
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-05-16 16:21:19 +0530

    Hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='hero container'>
+        <div className="heroText">
+            <h1>We ensure better education for a better world </h1>
+            <p>
+                Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education 
+            </p>
+            <button className='btn'>Explore More →</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
