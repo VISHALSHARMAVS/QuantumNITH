157267078c76df267be6aee382f084f4c15914d6
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-05-31 02:19:01 +0530

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,12 @@ import './Footer.css'
 function Footer() {
   return (
     <div className="footer">
-        <p>&#169; 2024 QUANTUMNITH. All rights are reserved</p>
+        <p>&#169; 2024 QUANTUMNITH. All rights are reserved
+          <br />
+         <div className='name'>
+         @Vishal Sharma 
+          </div> 
+        </p>
         <ul>
             <li>Terms of services </li>
             <li>Privacy Policy </li>
